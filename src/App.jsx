d97a9636b43c97b3b74ad83690bf41055e64e4ed
Debora Serra <debora.r.serra@gmail.com>
2025-07-09 14:53:59 -0700

    feat: add values section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Intro from "./components/intro";
 import Loading from "./components/loading";
 import Manifesto from "./components/manifesto";
 import ThemeProvider from "./utils/themeProvider";
+import Values from "./components/values";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Intro />
         <Manifesto />
         <AppIntro />
+        <Values />
         {/* <CookiesToast /> */}
       </Suspense>
     </ThemeProvider>
