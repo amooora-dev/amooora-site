f6b73a0e1dc6620ad409517866e5364f565e0a4b
Debora Serra <debora.r.serra@gmail.com>
2025-07-09 12:53:27 -0700

    feat: add the new component to existing sections

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,6 +1,5 @@
 import pessoas from "../../../assets/images/pessoas_roxo.jpg";
-import { cn } from "../../utils/cn";
-import { useTheme } from "../../utils/useTheme";
+import Content from "../content";
 
 const buttons = [
   {
@@ -29,94 +28,49 @@ const buttons = [
   },
 ];
 
-const AppIntro = () => {
-  const { theme } = useTheme();
-  const isLightTheme = theme === "light";
-
-  return (
-    <section id='aplicativo' className='py-10 px-3 md:px-0'>
-      <div className='container flex flex-col items-center justify-between gap-8 md:flex-row mx-auto relative'>
-        <div className='self-stretch min-w-[40%] w-[400px] md:static absolute left-0 top-0 opacity-40 md:opacity-100 max-w-full flex justify-center items-center'>
-          <img
-            src={pessoas}
-            alt='brejo'
-            className='rounded-lg shadow-lg'
-            width={388}
-            height={436}
-          />
-        </div>
-        <div className='grow flex flex-col items-start max-w-[750px] z-10'>
-          <p
-            className={cn(
-              "uppercase font-bold text-[0.85rem] mb-2",
-              isLightTheme ? "text-gray-700" : "text-gray-300"
-            )}
-          >
-            UM APLICATIVO SÁFICO
-          </p>
-          <h2
-            className={cn(
-              "text-[2.5rem] font-bold tracking-[-0.5px] mb-3",
-              isLightTheme ? "text-slate-800" : "text-slate-200"
-            )}
-          >
-            Mi brejo, su brejo
-          </h2>
-          <p
-            className={cn(
-              "mb-2 font-semibold",
-              isLightTheme ? "text-slate-700" : "text-slate-200"
-            )}
-          >
-            A gente quer se encontrar, trocar e se reconhecer. Criamos um espaço
+const paragraphs = [
+  `A gente quer se encontrar, trocar e se reconhecer. Criamos um espaço
             seguro, afetuoso e com a nossa cara para reunir toda a comunidade
-            sáfica.
-          </p>
-          <p
-            className={cn(
-              "mb-2 font-semibold",
-              isLightTheme ? "text-slate-700" : "text-slate-200"
-            )}
-          >
-            Mais do que visibilidade, a gente quer construir conexões. Trocas
+            sáfica.`,
+  `Mais do que visibilidade, a gente quer construir conexões. Trocas
             reais. Entretenimento, informação útil, apoio psicológico, saúde
-            íntima, orientação jurídica, e muito mais.
-          </p>
-          <p
-            className={cn(
-              "mb-2 font-semibold",
-              isLightTheme ? "text-slate-700" : "text-slate-200"
-            )}
-          >
-            As conexões aqui vão além do virtual! O virtual é a porta de
+            íntima, orientação jurídica, e muito mais.`,
+  `As conexões aqui vão além do virtual! O virtual é a porta de
             entrada, mas incentivamos as conexões no mundo real. Ou seja, o olho
             no olho, o toque na pele e a conversa ao pé de uma mesa sem hora
-            para acabar!
-          </p>
-          <p
-            className={cn(
-              "mb-2 font-semibold",
-              isLightTheme ? "text-slate-700" : "text-slate-200"
-            )}
-          >
-            Porque existem muitos brejos, e sempre vai ter um com a sua cara. A
-            Amooora oferece:
-          </p>
+            para acabar!`,
+  `Porque existem muitos brejos, e sempre vai ter um com a sua cara. A
+            Amooora oferece:`,
+];
 
-          <div className='flex flex-wrap items-center justify-center mt-4 mx-auto'>
-            {buttons.map((button, index) => (
-              <a
-                key={index}
-                href={button.href}
-                className='bg-[#932d6f] text-white px-4 py-2 rounded-lg shadow-md hover:opacity-80 transition-colors duration-300 mr-2 mb-2 w-[200px] uppercase font-semibold text-center'
-              >
-                {button.label}
-              </a>
-            ))}
-          </div>
+const AppIntro = () => {
+  return (
+    <Content
+      id='aplicativo'
+      upperTitle='UM APLICATIVO SÁFICO'
+      title='Mi brejo, su brejo'
+      paragraphs={paragraphs}
+      isImgLeft
+      img={{
+        src: pessoas,
+        alt: "brejo",
+        width: 388,
+        height: 436,
+      }}
+      extraContent={
+        <div className='flex flex-wrap items-center justify-center mt-4 mx-auto'>
+          {buttons.map((button, index) => (
+            <a
+              key={index}
+              href={button.href}
+              className='bg-[#932d6f] text-white px-4 py-2 rounded-lg shadow-md hover:opacity-80 transition-colors duration-300 mr-2 mb-2 w-[200px] uppercase font-semibold text-center'
+            >
+              {button.label}
+            </a>
+          ))}
         </div>
-      </div>
-    </section>
+      }
+    />
   );
 };
 
--- src/components/manifesto/index.jsx
+++ src/components/manifesto/index.jsx
@@ -1,54 +1,38 @@
 import apoia from "../../../assets/images/apoooia_site.jpg";
-import { cn } from "../../utils/cn";
-import { useTheme } from "../../utils/useTheme";
+import Content from "../content";
 
-const Manifesto = () => {
-  const { theme } = useTheme();
-   const isLightTheme = theme === "light";
-
-  return (
-    <section id='manifesto' className='py-10 px-3 md:px-0'>
-      <div className='container flex flex-col items-center justify-between gap-8 md:flex-row mx-auto relative'>
-        <div className='grow flex flex-col max-w-[750px] z-10'>
-          <p className={cn('uppercase font-bold text-[0.85rem] mb-2', isLightTheme ? 'text-gray-700' : 'text-gray-300')}>UM SONHO, UM IDEAL</p>
-          <h2 className={cn('text-[2.5rem] font-bold tracking-[-0.5px] mb-3', isLightTheme ?'text-slate-800':'text-slate-200')}>Manifesto</h2>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>Nós somos Amooora.</p>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>
-            Nascemos da urgência e do desejo de construir um mundo onde a
+const paragraphs = [
+  "Nós somos Amooora.",
+  `Nascemos da urgência e do desejo de construir um mundo onde a
             comunidade sáfica — mulheres lésbicas, bissexuais, pansexuais,
             pessoas trans e não binárias que se relacionam com outras
-            identidades femininas — possa se sentir livre, segura e pertencente.
-          </p>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>
-            Somos uma plataforma feita por nós, para nós — um espaço onde
+            identidades femininas — possa se sentir livre, segura e pertencente.`,
+  `Somos uma plataforma feita por nós, para nós — um espaço onde
             existências plurais não apenas cabem, mas são celebradas, validadas
-            e conectadas.
-          </p>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>
-            A Amooora é mais do que uma plataforma online! É a resposta a um
-            vazio histórico de visibilidade, cuidado e recursos voltados para a
-            comunidade sáfica.
-          </p>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>
-            É onde você pode ser quem é — sem medo, sem julgamentos, com
-            acolhimento.
-          </p>
-          <p className={cn("mb-2 font-semibold", isLightTheme ? 'text-slate-700' : 'text-slate-300')}>
-            É onde a gente se encontra para existir com coragem, afeto e em todo
-            o nosso potencial.
-          </p>
-        </div>
-        <div className='self-stretch min-w-[40%] w-[400px] md:static absolute right-0 top-0 opacity-50 md:opacity-100 max-w-full flex justify-center items-center'>
-          <img
-            className='rounded-lg shadow-lg'
-            src={apoia}
-            alt='serviços'
-            width={388}
-            height={436}
-          />
-        </div>
-      </div>
-    </section>
+            e conectadas.`,
+  `A Amooora é mais do que uma plataforma online! É a resposta a um
+              vazio histórico de visibilidade, cuidado e recursos voltados para a
+              comunidade sáfica.`,
+  `É onde você pode ser quem é — sem medo, sem julgamentos, com
+            acolhimento.`,
+  `É onde a gente se encontra para existir com coragem, afeto e em todo
+            o nosso potencial.`,
+];
+
+const Manifesto = () => {
+  return (
+    <Content
+      id='manifesto'
+      upperTitle='UM SONHO, UM IDEAL'
+      title='Manifesto'
+      paragraphs={paragraphs}
+      img={{
+        src: apoia,
+        alt: "Amooora Manifesto",
+        width: 388,
+        height: 436,
+      }}
+    />
   );
 };
 
