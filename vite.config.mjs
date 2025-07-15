b601e1c9d8493e82b4be925cef070b6bfaf1eb01
Debora Serra <debora.r.serra@gmail.com>
2025-07-15 14:56:12 -0700

    chore: update vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://amooora-dev.github.io/amooora-site"
+  base: "https://amooora-dev.github.io/"
 })
