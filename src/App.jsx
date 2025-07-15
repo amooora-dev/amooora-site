b8bd20d7a75b6f0e6bad171e3ea2f9833e6864b7
Debora Serra <debora.r.serra@gmail.com>
2025-07-15 13:25:46 -0700

    feat: add cookies consent

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Loading from "./components/loading";
 import Manifesto from "./components/manifesto";
 import Newsletter from "./components/newsletter";
 import Store from "./components/store";
+import CookiesToast from "./components/toast";
 import Values from "./components/values";
 import WhoAreWe from "./components/whoAreWe";
 import ThemeProvider from "./utils/themeProvider";
@@ -28,7 +29,7 @@ function App() {
         <WhoAreWe />
         <Newsletter />
         <Footer />
-        {/* <CookiesToast /> */}
+        <CookiesToast />
       </Suspense>
     </ThemeProvider>
   );
